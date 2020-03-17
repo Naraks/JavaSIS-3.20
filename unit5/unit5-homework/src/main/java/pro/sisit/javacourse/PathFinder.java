9190d366f71ab909bf720f8ccb1fcb6054c78581
Sergey Rudenko <progenitor123@gmail.com>
2020-03-18 05:56:44 +0700

    unit5_hw1

--- unit5/unit5-homework/src/main/java/pro/sisit/javacourse/PathFinder.java
+++ unit5/unit5-homework/src/main/java/pro/sisit/javacourse/PathFinder.java
@@ -1,19 +1,36 @@
 package pro.sisit.javacourse;
 
 import pro.sisit.javacourse.optimal.DeliveryTask;
+import pro.sisit.javacourse.optimal.Route;
+import pro.sisit.javacourse.optimal.RouteType;
 import pro.sisit.javacourse.optimal.Transport;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PathFinder {
 
     /**
      * Возвращает оптимальный транспорт для переданной задачи.
-     * Если deliveryTask равна null, то оптимальеый транспорт тоже равен null.
+     * Если deliveryTask равна null, то оптимальный транспорт тоже равен null.
      * Если список transports равен null, то оптимальеый транспорт тоже равен null.
      */
     public Transport getOptimalTransport(DeliveryTask deliveryTask, List<Transport> transports) {
-        // ToDo: realize me!
-        return null;
+        if (deliveryTask == null || transports == null) {
+            return null;
+        }
+
+        List<RouteType> deliveryTaskRouteTypes =
+                deliveryTask.getRoutes()
+                        .stream()
+                        .map(Route::getType)
+                        .distinct()
+                        .collect(Collectors.toList());
+        return transports.stream()
+                .filter(transport -> deliveryTaskRouteTypes.contains(transport.getType()))
+                .filter(transport -> transport.getVolume().compareTo(deliveryTask.getVolume()) >= 0)
+                .min(Comparator.comparing(Transport::getPrice))
+                .get();
     }
 }
