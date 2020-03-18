24025ce50b3324a785757caab9c4a3895c36be5d
Sergey Rudenko <progenitor123@gmail.com>
2020-03-19 06:55:31 +0700

    fix

--- unit5/unit5-homework/src/main/java/pro/sisit/javacourse/PathFinder.java
+++ unit5/unit5-homework/src/main/java/pro/sisit/javacourse/PathFinder.java
@@ -5,32 +5,51 @@ import pro.sisit.javacourse.optimal.Route;
 import pro.sisit.javacourse.optimal.RouteType;
 import pro.sisit.javacourse.optimal.Transport;
 
+import java.math.BigDecimal;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PathFinder {
 
     /**
      * Возвращает оптимальный транспорт для переданной задачи.
-     * Если deliveryTask равна null, то оптимальеый транспорт тоже равен null.
-     * Если список transports равен null, то оптимальеый транспорт тоже равен null.
+     * Если deliveryTask равна null, то оптимальный транспорт тоже равен null.
+     * Если список transports равен null, то оптимальный транспорт тоже равен null.
      */
     public Transport getOptimalTransport(DeliveryTask deliveryTask, List<Transport> transports) {
-        if (deliveryTask == null || transports == null) {
-            return null;
-        }
 
-        List<RouteType> deliveryTaskRouteTypes =
-                deliveryTask.getRoutes()
-                        .stream()
-                        .map(Route::getType)
-                        .distinct()
-                        .collect(Collectors.toList());
-        return transports.stream()
-                .filter(transport -> deliveryTaskRouteTypes.contains(transport.getType()))
-                .filter(transport -> transport.getVolume().compareTo(deliveryTask.getVolume()) >= 0)
-                .min(Comparator.comparing(Transport::getPrice))
-                .get();
+        Optional<DeliveryTask> optionalDeliveryTask = Optional.ofNullable(deliveryTask);
+        Optional<List<Transport>> optionalTransports = Optional.ofNullable(transports);
+        
+        if (optionalDeliveryTask.isPresent() && optionalTransports.isPresent()){
+            return transports.stream()
+                    .filter(transportSuitableByRouteTypePredicate(deliveryTask))
+                    .filter(transportSuitableByVolumePredicate(deliveryTask))
+                    .min(Comparator.comparing(transport -> getTransportPriceByDeliveryTask(deliveryTask, transport)))
+                    .orElse(null);
+        }
+        else return null;
+    }
+
+    private Predicate<Transport> transportSuitableByRouteTypePredicate(DeliveryTask deliveryTask) {
+        return transport -> deliveryTask.getRoutes().stream().anyMatch(route -> route.getType().equals(transport.getType()));
+    }
+
+    private Predicate<Transport> transportSuitableByVolumePredicate(DeliveryTask deliveryTask) {
+        return transport -> transport.getVolume().compareTo(deliveryTask.getVolume()) >= 0;
+    }
+
+    private BigDecimal getTransportPriceByDeliveryTask(DeliveryTask deliveryTask, Transport transport) {
+        return deliveryTask.getRoutes().stream()
+                .filter(route -> route.getType().equals(transport.getType()))
+                .findAny()
+                .map(route -> route.getLength().multiply(transport.getPrice()))
+                .orElse(null);
+
     }
 }
