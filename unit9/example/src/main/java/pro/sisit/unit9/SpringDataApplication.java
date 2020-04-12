96fe7224dfe02bbe9935c45ec1c48ca1e148bcc1
Sergey <Progenitor123@gmail.com>
2020-04-12 20:01:45 +0700

    hw

--- unit9/example/src/main/java/pro/sisit/unit9/SpringDataApplication.java
+++ unit9/example/src/main/java/pro/sisit/unit9/SpringDataApplication.java
@@ -2,10 +2,8 @@ package pro.sisit.unit9;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "pro.sisit.unit9")
 public class SpringDataApplication {
 
     public static void main(String[] args) {
